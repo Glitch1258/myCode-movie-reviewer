b1b2af102af92c7348256742d8e07b719be1e349
Glitch1258 <glitch12358@gmail.com.com>
2025-06-16 17:34:53 +0600

    4

--- services/tmdb.js
+++ services/tmdb.js
@@ -3,12 +3,21 @@ import axios from "axios"
 const apiKey = process.env.REACT_APP_TMDB_API_KEY;
 const base = "https://api.themoviedb.org/3";
 
-export const search = (q) = > {
+export const movieSearch = (q) = > {
  return axios.get(`${base}/search/movie`,{
    params:{
      key: apiKey,
 	 query: q,
    },
  });
-} ;
+}
+
+export const getDetails = (id) =>{
+ return axios.get(`${base}/movie/${id}`,{
+  params:{
+    key:apiKey,
+	categories:"credits,videos",
+  },
+ })
+}
 
